0197b5ac4a8cd07dc03ba52cbe1ef1cd6fee3a52
RamirezAldo <aldo.ramirez@revature.net>
2021-12-28 17:22:23 -0600

    HTTP request

---
+++ Demos/XmlHTTPRequest_Fetch/XhrFetch.js
@@ -0,0 +1,46 @@
+let oneJokeButton = document.createElement('button');
+document.body.appendChild(oneJokeButton);
+oneJokeButton.innerText = 'Push For Joke';
+
+let xhr = new XMLHttpRequest();
+oneJokeButton.addEventListener('click', (event) => {
+    xhr.onreadystatechange = () => {
+        console.log(`The ready state is ${xhr.readyState}`);
+
+        if(xhr.readyState == 4)
+        {
+            console.log(`The response is ${xhr.responseText}`);
+
+            //Code to render the joke only to the browser.
+            let myDiv = document.createElement('div');
+            let myPar = document.createElement('p');
+            myDiv.appendChild(myPar);
+            document.body.appendChild(myDiv);
+            let response = JSON.parse(xhr.responseText);
+            myPar.innerText = response.value.joke;
+        }
+    };
+    xhr.open('GET', 'http://api.icndb.com/jokes/random');
+    xhr.send();
+});
+
+let fiveJokeButton = document.createElement('button');
+document.body.appendChild(fiveJokeButton);
+fiveJokeButton.innerText = 'Push For 5 Jokes';
+let myDiv = document.createElement('div');
+let myPar = document.createElement('p');
+
+fiveJokeButton.onclick = () => {
+    fetch('http://api.icndb.com/jokes/random/5')
+    .then((res, err) => {
+        if(err) {
+            console.log(`There was an error in the request ${err}`);
+        }
+        else {
+            myDiv.appendChild(myPar);
+            document.body.appendChild(myDiv);
+            return res.json();
+        }
+    })
+    .then(res => myPar.innerText = res.value.joke);
+};
