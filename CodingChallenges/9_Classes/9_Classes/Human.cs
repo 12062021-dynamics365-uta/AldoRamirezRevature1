dbf2291e23ccf2d2a8bcd41991a8ebaf10ab26a4
RamirezAldo <aldo.ramirez@revature.net>
2021-12-29 13:13:36 -0600

    working on classes

--- CodingChallenges/9_Classes/9_Classes/Human.cs
+++ CodingChallenges/9_Classes/9_Classes/Human.cs
@@ -6,5 +6,23 @@ namespace _9_ClassesChallenge
 {
     internal class Human
     {
+        private string fName;
+        private string lName;
+
+        public Human ()
+        {
+            fName = "Pat";
+            fName = "Smyth";
+        }
+        public Human (string fName, string lName)
+        {
+            this.fName = fName;
+            this.fName = lName;
+        }
+
+        public string AboutMe()
+        {
+            return $"My name is {fName} {lName}.";
+        }
     }//end of class
 }//end of namespace
--- CodingChallenges/9_Classes/9_Classes/Human2.cs
+++ CodingChallenges/9_Classes/9_Classes/Human2.cs
@@ -1,11 +1,68 @@
 using System;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 [assembly: InternalsVisibleTo("9_ClassesChallenge.Tests")]
 namespace _9_ClassesChallenge
 {
     internal class Human2
     {
+        private string fName;
+        private string lName;
+        private string eyeColor;
+        private int age;
+        private int weight;
 
+        public int Weight { 
+            get {return weight; } 
+            set 
+            {
+                if (weight < 0 || weight > 400)
+                    weight = 0;
+            } 
+        }
+
+        public Human2()
+        {
+            fName = "Pat";
+            lName = "Smyth";
+        }
+        public Human2(string fName, string lName, string eyeColor = "", int age = 0)
+        {
+            this.fName = fName;
+            this.lName = lName;
+            this.eyeColor = eyeColor;
+            this.age = age;
+        }
+        public Human2(string fName, string lName, string eyeColor = "")
+        {
+            this.fName = fName;
+            this.lName = lName;
+            this.eyeColor = eyeColor;
+            this.age = 0;
+        }
+        public Human2(string fName, string lName, int age = 0)
+        {
+            this.fName = fName;
+            this.lName = lName;
+            this.eyeColor = "";
+            this.age = age;
+        }
+
+        public string AboutMe()
+        {
+            return $"My name is {fName} {lName}.";
+        }
+
+        public string AboutMe2()
+        {
+            if (age != 0 && eyeColor != "")
+                return ($"My name is {fName} {lName}. My age is {age}. My eye color is {eyeColor}.");
+            else if (eyeColor != "")
+                return ($"My name is {fName} {lName}. My eye color is {eyeColor}.");
+            else if (age != 0)
+                return ($"My name is {fName} {lName}. My age is {age}.");
+            return "";
+        }
     }
 }
