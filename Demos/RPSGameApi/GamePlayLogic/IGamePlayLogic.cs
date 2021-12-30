977a982d038abdaffab8fcc1ec4045460d21fdd3
RamirezAldo <aldo.ramirez@revature.net>
2021-12-30 13:32:19 -0600

    RPS Api Demo

---
+++ Demos/RPSGameApi/GamePlayLogic/IGamePlayLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace GamePlayLogic
+{
+    public interface IGamePlayLogic
+    {
+        Player WinnerYet();
+        List<Player> GetAllPlayers();
+        List<Game> PrintUsersGames();
+        void Login(string userFName, string userLName);
+        void StartNewGame();
+        void ResetGame();
+        Choice ValidateUserChoice(string choice);
+        Choice GetComputerChoice();
+        Player PlayRound(Choice p1Choice, Choice p2Choice);
+        int GetComputerWins();
+        int GetUserWins();
+        int GetTies();
+        int GetNumRounds();
+    }
+}
