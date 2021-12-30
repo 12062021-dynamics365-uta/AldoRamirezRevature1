f02ef7c43cb379f7f61e60ebf55e097d95f02cce
RamirezAldo <aldo.ramirez@revature.net>
2021-12-29 18:18:17 -0600

    Finished Coding Challenges

--- CodingChallenges/11_ArraysAndLists/11_ArraysAndLists/Program.cs
+++ CodingChallenges/11_ArraysAndLists/11_ArraysAndLists/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace _11_ArraysAndListsChallenge
 {
@@ -8,7 +9,36 @@ namespace _11_ArraysAndListsChallenge
     {
         public static void Main(string[] args)
         {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.Write("Array: ");
+            Array.ForEach(array, x => Console.Write($"{x} "));
+            Console.WriteLine($"\nAverage of array: {AverageOfValues(array)}\n");
 
+            array = SunIsShining(array);
+            Console.WriteLine("Every value in array incremented by 2: ");
+            Console.Write("Array: ");
+            Array.ForEach(array, x => Console.Write($"{x} "));
+
+            ArrayList myArrayList = new ArrayList() { 5, 1.1, 0, 2.2, "hellos", 9, 3 };
+            Console.Write("\n\nArrayList: ");
+            foreach (var i in myArrayList)
+                Console.Write($"{i} ");
+            Console.WriteLine($"\nAverage of array {ArrayListAvg(myArrayList)}\n");
+
+            List<int> list = new List<int>() { 9, 8, 7, 6, 5, 3, 2, 1 };
+            int newNum = 4;
+            Console.Write("List: ");
+            list.ForEach(x => Console.Write($"{x} "));
+            Console.WriteLine($"\nNumber to insert: {newNum}");
+            Console.WriteLine($"Rank of inserted number after sorting: {ListAscendingOrder(list, newNum)}\n");
+
+            List<string> words = new List<string>() { "Hi", "My", "Name", "is", "Aldo" };
+            string word1 = "Aldo";
+            string word2 = "Hello";
+            Console.Write("List: ");
+            words.ForEach(x => Console.Write($"{x} "));
+            Console.WriteLine($"\n'{word1}' found? {FindStringInList(words, word1)}");
+            Console.WriteLine($"'{word2}' found? {FindStringInList(words, word2)}");
         }//EoM
 
         /// <summary>
@@ -19,7 +49,7 @@ namespace _11_ArraysAndListsChallenge
         /// <returns></returns>
         public static double AverageOfValues(int[] array)
         {
-            throw new NotImplementedException("AverageOfValues has not been implemented yet.");
+            return array.Average();
         }
 
         /// <summary>
@@ -30,7 +60,9 @@ namespace _11_ArraysAndListsChallenge
         /// <returns></returns>
         public static int[] SunIsShining(int[] x)
         {
-            throw new NotImplementedException("SunIsShining has not been implemented yet.");
+            for (int i = 0; i < x.Length; i++)
+                x[i] += 2;
+            return x;
         }
 
         /// <summary>
@@ -42,7 +74,17 @@ namespace _11_ArraysAndListsChallenge
         /// <returns></returns>
         public static decimal ArrayListAvg(ArrayList myArrayList)
         {
-            throw new NotImplementedException("ArrayListAvg has not been implemented yet.");
+            decimal avg = 0;
+            int count = 0;
+            for (int i = 0; i < myArrayList.Count; i++)
+            {
+                if (myArrayList[i].GetType() != typeof(string))
+                {
+                    avg += Convert.ToDecimal(myArrayList[i]);
+                    count++;
+                }
+            }
+            return decimal.Round(avg / count, 3);
         }
 
         /// <summary>
@@ -52,7 +94,9 @@ namespace _11_ArraysAndListsChallenge
         /// <param name="myArray1"></param>
         public static int ListAscendingOrder(List<int> scores, int yourScore)
         {
-            throw new NotImplementedException("ListAscendingOrder has not been implemented yet.");
+            scores.Add(yourScore);
+            scores.Sort();
+            return scores.IndexOf(yourScore) + 1;
         }
 
         /// <summary>
@@ -64,7 +108,7 @@ namespace _11_ArraysAndListsChallenge
         /// <returns></returns>
         public static bool FindStringInList(List<string> myArray2, string word)
         {
-            throw new NotImplementedException("FindStringInList has not been implemented yet.");
+            return myArray2.Contains(word);
         }
     }//EoP
 }// EoN
